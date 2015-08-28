355c1ca0ec40621721bd6534154d9d74c1095b16
BikeshDahal <bdahal2358@gmail.com>
2015-08-27 21:00:34 -0400

    Finished lab

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,40 @@
 def division(num1, num2)
+    
+    div=num1/num2
+    div;
  
 end
 
 def assign_variable(value)
+    
+    vari=value;
   
 end
 
-def argue
+def argue(argument)
+    
+    return "#{argument}";
   
 end
 
-def greeting
+def greeting(argument1, argument2)
 
 end
 
 def return_a_value
+    
+    return "Nice"
 
 end
 
 def last_evaluated_value
+    
+    return "expert"
 
 end
 
-def pizza_party
+def pizza_party(argument="cheese")
+    
+    return"#{argument}"
 
 end
